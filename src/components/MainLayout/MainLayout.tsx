eb8cb63ae8857a8b8ea75229ed0919b581c57f5d
Mar Kal <marc@example.com>
2023-11-04 14:03:29 +0100

    feat: finish task 2.1

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/" underline="hover">
-        My Store
+        My photography store - markal84
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
