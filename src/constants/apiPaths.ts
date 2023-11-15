fc513d1ed46040d00d38318fea72bb6c9de4573a
Mar Kal <marc@example.com>
2023-11-15 17:00:33 +0100

    refactor: api product path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://bdjuch9dx2.execute-api.eu-north-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  task3: "https://bdjuch9dx2.execute-api.eu-north-1.amazonaws.com",
 };
 
 export default API_PATHS;
