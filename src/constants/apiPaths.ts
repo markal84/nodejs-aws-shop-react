cdc3294105230b93719148407dac01ed40232fc8
Mar Kal <marc@example.com>
2023-11-23 11:07:14 +0100

    feat: update import API PATCHS

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://bdjuch9dx2.execute-api.eu-north-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://z6eyy7j82i.execute-api.eu-north-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
